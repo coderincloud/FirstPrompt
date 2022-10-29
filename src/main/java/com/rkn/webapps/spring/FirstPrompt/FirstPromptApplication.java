9b8e4a1d075b2dc13f1b18c48472ef4ac4b8b258
Ravi Krishna Nittala <rkn@Ravis-MacBook-Pro.local>
2022-10-28 22:11:22 -0400

    FirstPrompt Checkin

---
+++ src/main/java/com/rkn/webapps/spring/FirstPrompt/FirstPromptApplication.java
@@ -0,0 +1,21 @@
+package com.rkn.webapps.spring.FirstPrompt;
+
+import org.springframework.boot.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.autoconfigure.*;
+
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class FirstPromptApplication {
+
+@RequestMapping("/")
+String home(){
+	return "First Prompt";
+}
+	public static void main(String[] args) {
+		SpringApplication.run(FirstPromptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkn/webapps/spring/FirstPrompt/FirstPromptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkn.webapps.spring.FirstPrompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstPromptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
